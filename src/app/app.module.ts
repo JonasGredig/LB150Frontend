2d08e5cbbe55f176b7926c751cc0d5ada076f287
Jonas Gredig <jongredig@gmail.com>
2020-01-19 15:27:33 +0100

    add basic project structure with components and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {EmployeelistComponent} from './employeelist/employeelist.component';
+import {DepartmentlistComponent} from './departmentlist/departmentlist.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeelistComponent,
+    DepartmentlistComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
